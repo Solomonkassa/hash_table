100683f2dbf9ce48bfd0d631df712d99d6adefc4
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-04-13 13:01:41 -0400

    Update island.py

--- island.py
+++ island.py
@@ -22,3 +22,22 @@ def removeIsland(matrix):
     
     # Return the modified matrix
     return matrix
+   def main():
+    matrix = [[1, 0, 0, 1, 0],
+              [0, 1, 0, 0, 1],
+              [0, 0, 1, 1, 0],
+              [1, 0, 0, 0, 1]]
+    
+    print("Original matrix:")
+    printMatrix(matrix)
+    
+    matrix = removeIsland(matrix)
+    
+    print("Modified matrix:")
+    printMatrix(matrix)
+    
+def printMatrix(matrix):
+    for row in matrix:
+        print(row)
+
+main()
